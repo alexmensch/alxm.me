243c78807b494f2d154f7f8e9631af9a3465c173
Alex Marshall <alexmensch@users.noreply.github.com>
2024-07-10 12:39:24 +0100

    CF Pages font config - debug

--- cf-build.sh
+++ cf-build.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 export FONTCONFIG_FILE=`pwd`/fonts.conf
 
+cat <<EOF > fonts.conf
+<?xml version="1.0"?>
+<!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
+<fontconfig>
+  <dir>/opt/buildhome/repo/src/assets/fonts/inter/</dir>
+   <cachedir>/tmp/fonts-cache/</cachedir>
+   <config></config>
+</fontconfig>
+EOF
+
 # Debug stuff
 fc-list
 
