f9e7bdcf92bb375402a1cfa2f39aca74a7cb0325
Alex Marshall <alexmensch@users.noreply.github.com>
2024-07-10 13:25:11 +0100

    CF Pages font config - debug

--- cf-build.sh
+++ cf-build.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
-export FONTCONFIG_FILE=`pwd`/fonts.conf
+export FONTCONFIG_FILE=`pwd`/_build/fonts.conf
 
-cat <<EOF > fonts.conf
+cat <<EOF > _build/fonts.conf
 <?xml version="1.0"?>
 <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
 <fontconfig>
-  <dir>/opt/buildhome/repo/src/assets/fonts/inter/</dir>
-   <cachedir>/tmp/fonts-cache/</cachedir>
-   <config></config>
+  <dir>`pwd`/_build/fonts/</dir>
+  <cachedir>/tmp/fonts-cache/</cachedir>
+  <config></config>
 </fontconfig>
 EOF
 
-# DEBUG
-fc-match Inter Variable
-
 sass src/assets/scss/main.scss src/assets/css/main.css
 npx @11ty/eleventy
-
-rm fonts.conf
