72dfd3d3a84572cb402ecda0e16f77b69713bb31
Alex Marshall <git@alxm.me>
2025-08-14 21:53:02 +0100

    Permalink tracking plugin and baseline output file

---
+++ eleventy-plugins/permalink-tracker.js
@@ -0,0 +1,35 @@
+import fs from 'fs';
+import path from 'path';
+
+export default function permalinkTrackerPlugin(eleventyConfig) {
+  let collectedPermalinks = new Set();
+  
+  eleventyConfig.addTransform("collectPermalinks", function(content, outputPath) {
+    if (outputPath) {
+      let permalink = outputPath.replace(/^.*\/_site/, '');
+      if (!permalink.startsWith('/')) {
+        permalink = '/' + permalink;
+      }
+      
+      if (outputPath.endsWith('.html')) {
+        permalink = permalink.replace(/\/index\.html$/, '/');
+        if (permalink.endsWith('.html') && permalink !== '/') {
+          permalink = permalink.replace(/\.html$/, '');
+        }
+      }
+      
+      collectedPermalinks.add(permalink);
+    }
+    return content;
+  });
+  
+  eleventyConfig.on('eleventy.after', async () => {
+    const sortedPermalinks = Array.from(collectedPermalinks).sort();
+    
+    const permalinksFile = path.join(process.cwd(), 'permalinks.txt');
+    const content = sortedPermalinks.join('\n') + '\n';
+    
+    fs.writeFileSync(permalinksFile, content, 'utf8');
+    console.log(`✅ Wrote ${sortedPermalinks.length} permalinks to permalinks.txt`);
+  });
+}
