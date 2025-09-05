6c07a0e911faf48c9035021c2b56b6b42017c491
Alex Marshall <git@alxm.me>
2025-09-05 18:14:13 +0100

    Updated Felix episode with normalised mastering; updated last update header for podcast feed

--- _cloudflare/config.js
+++ _cloudflare/config.js
@@ -5,5 +5,5 @@ export const config = {
   WORKER_NAME: "alxm-proxy",
   DOMAIN: "alxm.me",
   RSS_PATH: "/podcast/feed/what-i-knew.rss",
-  RSS_LAST_MODIFIED: "2025-08-02" // YYYY-MM-DD format
+  RSS_LAST_MODIFIED: "2025-09-05" // YYYY-MM-DD format
 };
