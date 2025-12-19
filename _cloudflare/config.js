87499057255c5f17d38ae067d8b2452f0d94e819
Alex Marshall <git@alxm.me>
2025-12-19 18:48:29 +0000

    Refresh podcast RSS feed (#143)

--- _cloudflare/config.js
+++ _cloudflare/config.js
@@ -5,5 +5,5 @@ export const config = {
   WORKER_NAME: "alxm-proxy",
   DOMAIN: "alxm.me",
   RSS_PATH: "/podcast/feed/what-i-knew.rss",
-  RSS_LAST_MODIFIED: "2025-11-10" // YYYY-MM-DD format
+  RSS_LAST_MODIFIED: "2025-12-19" // YYYY-MM-DD format
 };
